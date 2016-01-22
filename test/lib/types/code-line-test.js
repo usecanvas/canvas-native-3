4fb87ed7d9d8bfc4f6c165fca60266e727d4f895
Jonathan Clem <jotclem@gmail.com>
2016-01-22 14:57:00 -0500

    Add grouping for CodeLine

--- test/lib/types/code-line-test.js
+++ test/lib/types/code-line-test.js
@@ -3,6 +3,17 @@ import Paragraph  from '../../../lib/types/paragraph';
 import { expect } from 'chai';
 
 describe('Type: CodeLine', () => {
+  describe('.matchMarkdown', () => {
+    it('matches code in a code group', () => {
+      expect(CodeLine.matchMarkdown('Code', { groupType: 'code' }))
+        .to.be.an.instanceof(CodeLine);
+    });
+
+    it('does not match code not in a code group', () => {
+      expect(CodeLine.matchMarkdown('Code')).to.be.null;
+    });
+  });
+
   describe('.matchNative', () => {
     const matchSource = CodeLine.buildNative('Foo', { language: 'ruby' });
 
@@ -41,7 +52,7 @@ describe('Type: CodeLine', () => {
     });
   });
 
-  describe.only('#toMarkdown', () => {
+  describe('#toMarkdown', () => {
     let line;
 
     beforeEach(() => {
