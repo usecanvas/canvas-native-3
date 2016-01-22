18cda3d32151b41f2804aa26b016d88e34681c6c
Jonathan Clem <jotclem@gmail.com>
2016-01-22 14:37:30 -0500

    Add ChecklistItem

---
+++ test/lib/types/checklist-item-test.js
@@ -0,0 +1,68 @@
+import ChecklistItem from '../../../lib/types/checklist-item';
+import Paragraph     from '../../../lib/types/paragraph';
+import { expect }    from 'chai';
+
+describe('Type: ChecklistItem', () => {
+  [
+    ['matchMarkdown', '- [x] Foo'],
+    ['matchNative', ChecklistItem.buildNative('Foo', { checked: true })],
+  ].forEach(([matchType, matchSource]) => {
+    describe(`.${matchType}`, () => {
+      let line;
+
+      beforeEach(() => {
+        line = ChecklistItem[matchType](matchSource);
+      });
+
+      it('has a source', () => {
+        expect(line.source).to
+          .eq(ChecklistItem.buildNative('Foo', { checked: true }));
+      });
+
+      it('has content', () => {
+        expect(line.content).to.eql('Foo');
+      });
+
+      it('has metadata', () => {
+        expect(line.meta).to.eql({
+          checked: true,
+        });
+      });
+    });
+  });
+
+  describe('#toJSON', () => {
+    it('returns its type and content', () => {
+      const line = ChecklistItem.matchMarkdown('- [X] Foo');
+      expect(line.toJSON()).to.eql({
+        type: 'checklist-item',
+        content: 'Foo',
+        meta: { checked: true },
+      });
+    });
+  });
+
+  describe('#toMarkdown', () => {
+    let line;
+
+    beforeEach(() => {
+      line = ChecklistItem
+        .matchNative(ChecklistItem.buildNative('Foo', { checked: true }));
+    });
+
+    it('appends a new line at the end of a group', () => {
+      expect(line.toMarkdown(null, Paragraph.matchMarkdown('Foo')))
+        .to.eq('- [x] Foo\n');
+    });
+
+    it('does not append a newline mid-list', () => {
+      expect(line.toMarkdown(null, line))
+        .to.eql('- [x] Foo');
+    });
+
+    it('does not append a new line at the end of the document', () => {
+      expect(line.toMarkdown(null, null))
+        .to.eql('- [x] Foo');
+    });
+  });
+});
