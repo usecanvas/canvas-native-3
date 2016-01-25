9def97089f72514acc0eca0fec67a9c8451f5d5d
Jonathan Clem <jotclem@gmail.com>
2016-01-25 12:36:47 -0500

    Add Parser.Native

---
+++ dist/lib/parsers/native.js
@@ -0,0 +1,94 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _parseOrder = require('./parse-order');
+
+var _parseOrder2 = _interopRequireDefault(_parseOrder);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * A module that exposes a single function for parsing CanvasNative test into
+ * line objects
+ *
+ * @module Parsers.Native
+ */
+exports.default = {
+  parse: parse
+};
+
+/**
+ * Parse a CanvasNative-formatted string and return an array of line objects.
+ *
+ * @function
+ * @param {string} native The native text to parse
+ * @return {Array<Type>} An array of CanvasNative line objects
+ */
+
+function parse(native) {
+  var sourceLines = native.split('\n');
+  var result = [];
+
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = sourceLines[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var sourceLine = _step.value;
+
+      var line = undefined;
+
+      var _iteratorNormalCompletion2 = true;
+      var _didIteratorError2 = false;
+      var _iteratorError2 = undefined;
+
+      try {
+        for (var _iterator2 = _parseOrder2.default[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+          var klass = _step2.value;
+
+          if (line = klass.matchNative(sourceLine)) {
+            break;
+          }
+        }
+      } catch (err) {
+        _didIteratorError2 = true;
+        _iteratorError2 = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion2 && _iterator2.return) {
+            _iterator2.return();
+          }
+        } finally {
+          if (_didIteratorError2) {
+            throw _iteratorError2;
+          }
+        }
+      }
+
+      if (!line) {
+        throw new Error('No matching type for native source "' + sourceLine + '"');
+      }
+
+      result.push(line);
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  return result;
+}
