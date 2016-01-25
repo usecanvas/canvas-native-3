13ee09968d750cc68c67699fc22d941aa5647198
Jonathan Clem <jotclem@gmail.com>
2016-01-25 12:25:01 -0500

    Add parsing order

---
+++ dist/lib/parsers/parser-order.js
@@ -0,0 +1,53 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _blockquoteLine = require('../types/blockquote-line');
+
+var _blockquoteLine2 = _interopRequireDefault(_blockquoteLine);
+
+var _checklistItem = require('../types/checklist-item');
+
+var _checklistItem2 = _interopRequireDefault(_checklistItem);
+
+var _codeLine = require('../types/code-line');
+
+var _codeLine2 = _interopRequireDefault(_codeLine);
+
+var _heading = require('../types/heading');
+
+var _heading2 = _interopRequireDefault(_heading);
+
+var _horizontalRule = require('../types/horizontal-rule');
+
+var _horizontalRule2 = _interopRequireDefault(_horizontalRule);
+
+var _image = require('../types/image');
+
+var _image2 = _interopRequireDefault(_image);
+
+var _linkDefinition = require('../types/link-definition');
+
+var _linkDefinition2 = _interopRequireDefault(_linkDefinition);
+
+var _orderedListItem = require('../types/ordered-list-item');
+
+var _orderedListItem2 = _interopRequireDefault(_orderedListItem);
+
+var _paragraph = require('../types/paragraph');
+
+var _paragraph2 = _interopRequireDefault(_paragraph);
+
+var _title = require('../types/title');
+
+var _title2 = _interopRequireDefault(_title);
+
+var _unorderedListItem = require('../types/unordered-list-item');
+
+var _unorderedListItem2 = _interopRequireDefault(_unorderedListItem);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = [_checklistItem2.default, _blockquoteLine2.default, _codeLine2.default, _title2.default, _heading2.default, _horizontalRule2.default, _image2.default, _linkDefinition2.default, _orderedListItem2.default, _unorderedListItem2.default, _paragraph2.default];
